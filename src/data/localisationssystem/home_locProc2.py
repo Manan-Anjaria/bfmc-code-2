7c286685653e0dd1b319b313eddb76eb7a75c649
Manan-Anjaria <anjariamanan01@gmail.com>
2023-04-12 16:54:06 +0530

    fixing home loc

--- src/data/localisationssystem/home_locProc2.py
+++ src/data/localisationssystem/home_locProc2.py
@@ -1,30 +1,29 @@
+import json
+import socket
+from threading import Thread
+# from ..templates.workerprocess import WorkerProcess
+from src.templates.workerprocess import WorkerProcess
+# from ...templates.workerprocess import WorkerProcess
+import time
 from typing import Tuple
 import cv2
 import requests
 import numpy as np
-import time
-import json
-import socket
-from threading import Thread
 import zmq
-
 import joblib
 
 PI_IP = "192.168.152.242"
 PORT = 8888
+REMOTE_PORT = 8888
 
 
 def localize(img: np.ndarray) -> Tuple[float, float]:
     AREA_THRES = 100.0
-    # AREA_THRES = 50.0
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     frame_HSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    # cv2.imshow('frame HSV', frame_HSV)
-    # print(frame_HSV[521,493])
     frame_threshold = cv2.inRange(img, (35, 100, 150), (80, 160, 240))
     cnts = cv2.findContours(
         frame_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # print(cnts)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     x = None
     y = None
@@ -64,40 +63,51 @@ def annotate_image(x: float, y: float, image: np.ndarray) -> np.ndarray:
     )
 
 
-class LocalisationServer:
-    """Home Localisation Server"""
+class LocalisationProcess(WorkerProcess):
+    # ===================================== INIT =========================================
+    def __init__(self, inPs, outPs):
+        super(LocalisationProcess, self).__init__(inPs, outPs)
 
-    def __init__(self, preview=False) -> None:
-
+    # ===================================== RUN ==========================================
+    def run(self, preview=False):
         self.preview = preview
         self.port = PORT
         self.serverIp = PI_IP  # pi addr
         self.threads = list()
         self._init_socket()
+        super(LocalisationProcess, self).run()
 
+    # ===================================== INIT SOCKET ==================================
     def _init_socket(self):
-        """Initialize the communication socket client."""
-        self.client_socket = socket.socket(
+        self.port = REMOTE_PORT
+        self.serverIp = "0.0.0.0"
+
+        self.server_socket = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_DGRAM
         )
+        self.server_socket.bind((self.serverIp, self.port))
 
-    def run(self):
-        """Obtains image, applies the required image processing and computes the steering angle value.
+    # ===================================== INIT THREADS =================================
+    def _init_threads(self):
+        readTh = Thread(
+            name="LocSysRecvThread", target=self._read_stream, args=(self.outPs,)
+        )
+        self.threads.append(readTh)
 
-        Parameters
-        ----------
-        inP  : Pipe
-            Input pipe to read the frames from other process.
-        outP : Pipe
-            Output pipe to send the steering angle value to other process.
-        """
+    # ===================================== READ STREAM ==================================
+    def _read_stream(self, outPs):
 
+        # self.server_socket.setblocking(False)
         context_send = zmq.Context()
-        pub_hl = context_send.socket(zmq.PUB)
-        pub_hl.setsockopt(zmq.CONFLATE, 1)
-        pub_hl.bind("ipc:///tmp/vhl")
+        pub_loc = context_send.socket(zmq.PUB)
+        pub_loc.bind("ipc:///tmp/v31")
 
-        print("Started Home Localization System")
+        context_recv = zmq.Context()
+        sub_loc = context_recv.socket(zmq.SUB)
+        sub_loc.setsockopt(zmq.CONFLATE, 1)
+        sub_loc.connect("ipc:///tmp/vhl")
+        sub_loc.setsockopt_string(zmq.SUBSCRIBE, "")
+
         count = 0
         skip_count = 24
         r = requests.get(
@@ -105,7 +115,7 @@ class LocalisationServer:
         )
         rx = []
         ry = []
-        bytes1 = bytes()  # buffer
+        bytes1 = bytes()
         if r.status_code == 200:
             for idx, chunk in enumerate(r.iter_content(chunk_size=100_000)):
                 start_time = time.time()
@@ -121,32 +131,21 @@ class LocalisationServer:
                 jpg = bytes1[a: b + 2]  # get frame based on markers
                 bytes1 = bytes1[c + 2:]  # update buffer to store data
                 # of last frame present in chunk
-                # i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                i = cv2.imdecode(np.frombuffer(
-                    jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                i = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
                 # specify desired output size
                 width = 720
                 height = 1280
-                # specify conjugate x,y coordinates (not y,x)
-                # input = np.float32([[61, 345], [616, 35], [1279, 51], [870, 676]])
-                # input = np.float32([[0, 366], [582, 51], [1257, 66], [788, 715]])
                 input = np.float32(
                     [[38, 344], [617, 38], [1279, 57], [860, 669]])
-
                 output = np.float32(
                     [[0, 0], [width - 1, 0], [width - 1, width - 1], [0, width - 1]]
                 )
-
-                # compute perspective matrixbytes1 = bytes1[b+2:]
-                # i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
                 i = cv2.imdecode(np.frombuffer(
                     jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
                 # specify desired output size
                 width = 720
                 # height = 1280
                 matrix = cv2.getPerspectiveTransform(input, output)
-
-                # do perspective transformation setting area outside input to black
                 image = cv2.warpPerspective(
                     i,
                     matrix,
@@ -156,10 +155,7 @@ class LocalisationServer:
                     borderValue=(0, 0, 0),
                 )
                 x, y = localize(image)
-                # print(x,y)
                 if x and y:
-                    # rx.append(x)
-                    # ry.append(y)
                     data = {
                         "timestamp": time.time(),
                         "posA": x,
@@ -167,9 +163,7 @@ class LocalisationServer:
                         "rotA": 0,
                     }
                     data = json.dumps(data).encode()
-                    #data = data.decode('utf-8')
-                    self.client_socket.sendto(data, (self.serverIp, self.port))
-                    #pub_hl.send_json(data, flags=zmq.NOBLOCK)
+                    # data = data.decode('utf-8')
                     print(data)
                     imgOutput = annotate_image(x, y, image)
                 else:
@@ -178,7 +172,7 @@ class LocalisationServer:
                     cv2.imshow("Track Image", imgOutput)
                     key = cv2.waitKey(1)
                     if key == 27 or key == 113:
-                        joblib.dump({"x": rx, "y": ry}, "coordlist.z")
+                        # joblib.dump({"x": rx, "y": ry}, "coordlist.z")
                         cv2.destroyAllWindows()
                         break
                     else:
@@ -187,8 +181,22 @@ class LocalisationServer:
                         pass
         else:
             print("Received unexpected status code {}".format(r.status_code))
+        
+        print("------------REACHED BEFORE TRY----------------")
+        try:
+            print("Starting Home Localization Process")
+            while True:
+                print("-------------REACHED HERE---------------------")
+                # bts, addr = self.server_socket.recvfrom(1024)
+                # data = sub_loc.recv()
+                print(data)
+                # data = data.decode()
+                data = json.loads(data)
+                pub_loc.send_json(data, flags=zmq.NOBLOCK)
 
+        except Exception as e:
+            print("Home LocSys Error")
+            print(e)
 
-if __name__ == "__main__":
-    loc = LocalisationServer(preview=True)
-    loc.run()
+        finally:
+            self.server_socket.close()
